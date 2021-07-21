3a538a726f6edfeaa11164e6526dd9f0aad6d03c
Marknjo <marknjo7@gmail.com>
2021-07-21 08:41:10 +0300

    Recreating callback hell in NodeJs

--- index.js
+++ index.js
@@ -20,3 +20,39 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
 });
 
 console.log("Will read file!");
+
+console.log("\n");
+
+const checkErr = (err) => {
+  if (err) throw err;
+};
+
+fs.readFile("./txt/start.txt", "utf-8", (err, fileName) => {
+  checkErr(err);
+
+  fs.readFile(`./txt/${fileName}.txt`, "utf-8", (err, data) => {
+    checkErr(err);
+    console.log(data);
+
+    fs.readFile("./txt/append.txt", "utf-8", (err, appendTxt) => {
+      checkErr(err);
+      console.log(appendTxt);
+      console.log("Your file has been written");
+
+      fs.writeFile(
+        "./txt/final.txt",
+        `${data}\n${appendTxt}`,
+        "utf-8",
+        (err) => {
+          checkErr(err);
+
+          fs.readFile("./txt/final.txt", "utf-8", (err, mergedText) => {
+            checkErr(err);
+
+            console.log(mergedText);
+          });
+        }
+      );
+    });
+  });
+});
