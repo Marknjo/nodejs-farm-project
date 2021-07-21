ba072c85558dc4440d5cec92b1df32d15c7aa201
Marknjo <marknjo7@gmail.com>
2021-07-21 08:28:22 +0300

    Asynchronous reading files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+//Blocking synchronous way
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
 console.log(textIn);
@@ -9,3 +10,13 @@ const textOut = `This is what we know about the avocado: ${textIn}. \n Created o
 fs.writeFileSync("./txt/output.txt", textOut);
 
 console.log("File output file written...");
+console.log("\n");
+
+//Non-blocking asyncronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+
+  console.log(data);
+});
+
+console.log("Will read file!");
